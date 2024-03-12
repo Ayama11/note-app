feb6ca832091caf282738041cb15f3c6e3440945
Ayama11 <ayamalla882@gmail.com>
2024-03-12 22:53:50 +0300

    app bar and add widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
+import 'package:noteapp/views/note_view.dart';
 
 void main() {
   runApp(const NoteApp());
@@ -10,7 +12,9 @@ class NoteApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: F,
       theme: ThemeData.dark(),
+      home: const NoteView(),
     );
   }
 }
--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:noteapp/views/widgets/appbar.dart';
+
+import 'widgets/curculer_icons.dart';
 
 class NoteView extends StatelessWidget {
   const NoteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          AppBarW(
+            backgroundColor: Colors.transparent,
+            titel: const Text('Notes'),
+            actions: [
+              CircularIcons(
+                backgrounColor: Colors.grey,
+                height: 35,
+                width: 48,
+                size: 28,
+                color: Colors.white,
+                icon: Iconsax.search_normal,
+                onPreessed: () {},
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
