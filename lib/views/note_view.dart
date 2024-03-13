428e3fcbdf4d4e0c1471261ba059c1e529100bef
Ayama11 <ayamalla882@gmail.com>
2024-03-13 11:55:37 +0300

    create note iteam

--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -3,6 +3,7 @@ import 'package:iconsax/iconsax.dart';
 import 'package:noteapp/views/widgets/appbar.dart';
 
 import 'widgets/curculer_icons.dart';
+import 'widgets/note_item.dart';
 
 class NoteView extends StatelessWidget {
   const NoteView({super.key});
@@ -10,24 +11,30 @@ class NoteView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          AppBarW(
-            backgroundColor: Colors.transparent,
-            titel: const Text('Notes'),
-            actions: [
-              CircularIcons(
-                backgrounColor: Colors.grey,
-                height: 35,
-                width: 48,
-                size: 28,
-                color: Colors.white,
-                icon: Iconsax.search_normal,
-                onPreessed: () {},
-              )
-            ],
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.only(left: 16, right: 16),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 12),
+              child: AppBarW(
+                backgroundColor: Colors.transparent,
+                titel: const Text('Notes'),
+                actions: [
+                  CircularIcons(
+                      backgrounColor: Colors.grey.withOpacity(.4),
+                      height: 30,
+                      width: 50,
+                      size: 26,
+                      color: Colors.white,
+                      icon: Iconsax.search_normal,
+                      onPreessed: () {})
+                ],
+              ),
+            ),
+            const NoteIteam(),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/views/widgets/note_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class NoteIteam extends StatelessWidget {
+  const NoteIteam({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: Colors.blue,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Padding(
+              padding: EdgeInsets.only(left: 8, top: 16, bottom: 16),
+              child: Text('Flutter Notes'),
+            ),
+            subtitle: const Padding(
+              padding: EdgeInsets.only(top: 8, left: 8, bottom: 16),
+              child: Text('Build you carer with tharwat'),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Iconsax.trash,
+                  size: 26,
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8),
+            child: Text(
+              'Mars, 13/2024',
+              style: TextStyle(color: Colors.black.withOpacity(0.5)),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
