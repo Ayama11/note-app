87236402598798e4d7522c9e916bfdf7ea8f6b9e
Ayama11 <ayamalla882@gmail.com>
2024-03-14 23:14:10 +0300

    add custom buttun

--- lib/views/widgets/add_note_bottomsheet.dart
+++ lib/views/widgets/add_note_bottomsheet.dart
@@ -8,15 +8,20 @@ class BottomSheetNote extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 18),
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-          CustomTextField(
-            hint: 'Titel',
-          ),
-          SizedBox(height: 16),
-          CustomTextField(hint: 'content', maxline: 4),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32),
+            CustomTextField(
+              hint: 'Titel',
+            ),
+            SizedBox(height: 24),
+            CustomTextField(hint: 'content', maxline: 4),
+            SizedBox(height: 60),
+            CustomButton(),
+            SizedBox(height: 44),
+          ],
+        ),
       ),
     );
   }
@@ -43,5 +48,24 @@ class CustomTextField extends StatelessWidget {
     return OutlineInputBorder(
         borderRadius: BorderRadius.circular(30),
         borderSide: BorderSide(color: color ?? Colors.white));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 55,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+          color: kPrimaryColor, borderRadius: BorderRadius.circular(32)),
+      child: const Center(
+          child: Text(
+        'Add',
+        style: TextStyle(color: Colors.black, fontSize: 20),
+      )),
+    );
   }
 }
