ab65fe8a7666c550baa05421bf26fcbc319c5938
Ayama11 <ayamalla882@gmail.com>
2024-03-15 13:13:29 +0300

    add generator

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,1 +1,21 @@
-class NoteModel {}
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  final String titel;
+  @HiveField(1)
+  final String content;
+  @HiveField(2)
+  final String date;
+  @HiveField(3)
+  final int color;
+
+  NoteModel(
+      {required this.titel,
+      required this.content,
+      required this.date,
+      required this.color});
+}
