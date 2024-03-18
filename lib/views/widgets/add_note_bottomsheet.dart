be3f3689c4ee8be6d9c245a45d9fb4ca60da4b6e
Ayama11 <ayamalla882@gmail.com>
2024-03-18 12:45:45 +0300

    add if state to bottom cheet + modal prohud

--- lib/views/widgets/add_note_bottomsheet.dart
+++ lib/views/widgets/add_note_bottomsheet.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:noteapp/cubit/add_note/add_note_cubit.dart';
 import 'package:noteapp/cubit/add_note/add_note_state.dart';
 import 'package:noteapp/views/widgets/custom_buttun.dart';
 import 'package:noteapp/views/widgets/custom_textfiled.dart';
 import 'package:noteapp/views/widgets/form_bottom_sheet.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class BottomSheetNote extends StatelessWidget {
   const BottomSheetNote({super.key});
@@ -15,9 +17,18 @@ class BottomSheetNote extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 18),
       child: SingleChildScrollView(
         child: BlocConsumer<AddNoteCubit, AddNoteState>(
-          listener: (context, state) {},
+          listener: (context, state) {
+            if (state is AddNoteSucsse) {
+              navigator?.pop(context);
+            }
+            if (state is AddNoteFailure) {
+              print('erorr' "${state.errMesage}");
+            }
+          },
           builder: (context, state) {
-            return const FormInput();
+            return ModalProgressHUD(
+                inAsyncCall: state is AddNoteLoding ? true : false,
+                child: const FormInput());
           },
         ),
       ),
